04d64dc1c3deff95a1f77c9ea9e2d64445c3d463
ThomasDelezenne <91311655+ThomasDelezenne@users.noreply.github.com>
2022-11-28 18:17:59 +0100

    Add files via upload

---
+++ site marchand/index.php
@@ -0,0 +1,21 @@
+<?php
+include ("Fonctions.php");
+echo'
+    <!DOCTYPE html>
+    <html lang = "fr">
+        <head>
+            <title>SiteTech:acceuil</title>
+        </head>
+        <body>
+            <header>
+                <h1>Site Tech</h1>
+                <nav>
+                    <a href="index.php">acceuil</a>
+                    <a href="produit.php">nos produits</a>
+                    <a href="pannier.php">pannier</a>
+                </nav>
+            </header>
+        </body>
+    </html>
+';
+?>
---
+++ site marchand/peripherique.php
@@ -0,0 +1,25 @@
+<?php
+    include ("Fonctions.php");
+    echo'
+        <!DOCTYPE html>
+        <html lang="fr">
+            <head>
+                <title>SiteTech: peripherique</title>
+            </head>
+            <body>
+                <div id="app">
+                <header>
+                    <h1>SiteTch!</h1>
+                    <nav>
+                        <a href="index.html">acceuil</a>
+                        <a href="produit.html">nos produits</a> 
+                        <a href="pannier.html">pannier</a>
+                    </nav>
+                </header> 
+
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+            </body>
+        </html>
+    ';
+?>
